5b6c69c3a84b3762e8aca31386028e9f4b261450
mm1089 <michael@mclain.co.uk>
2022-04-25 15:49:40 +0100

    <edit> Addressed Issue #1 comments & minor changes

--- index.php
+++ index.php
@@ -4,15 +4,35 @@
         <title>PHP Assignment</title>
     </head>
 
+    <!-- Main Section for Implementation -->
+    <!-- Information on assignment etc. -->
+    
     <main class="">
         <h1>Assignment Brief</h1>
+        <p>
+            <ul>
+                <ol>
+                    <p>Item 1</p>
+                    <p>Item 2</p>
+                    <p>Item 3</p>
+                    <p>Item 4</p>
+                </ol>
+            </ul>
+        </p>
     </main>
 
     <body class="">
         <h1>Assignment PHP Index</h1>
+
+    <!-- PHP Implementation -->
+
     <?php
-    echo "Hello World!";
+    echo "This Assignment is due in 3 weeks!";
     ?>
-        <h2>Assignment</h2>
+        <h2>Assignment DB + PHP</h2>
     </body>
 </html>
+
+<!-- Resources Used -->
+<!-- http://php.net/manual/en/faq.installation.php#faq.installation.addtopath -->
+<!-- For Issue #1 'bash: php: command not found' -->
