e664cb03da2b843b6e614cd2df9706a37831bbf2
mm1089 <michael@mclain.co.uk>
2022-04-25 15:39:47 +0100

    <created> PHP Index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Assignment</title>
+    </head>
+
+    <main class="">
+        <h1>Assignment Brief</h1>
+    </main>
+
+    <body class="">
+        <h1>Assignment PHP Index</h1>
+    <?php
+    echo "Hello World!";
+    ?>
+        <h2>Assignment</h2>
+    </body>
+</html>
