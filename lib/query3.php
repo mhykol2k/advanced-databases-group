a5987cc06fbcaf529d763804f471cf28a79663fb
JS1240 <72129202+JS1240@users.noreply.github.com>
2022-05-11 17:04:32 +0100

    Created three php files for queries

---
+++ lib/query3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add New Record</title>
+        <link rel="stylesheet" href="../assets/main.css">
+        <link rel="icon" href="../media/globe-icon.png">
+    </head>
+<!-- Navigation bar -->
+    <main>
+          <div class="sidenav">
+            <a href="index.php">Return to Home</a>
+            <a href="query1.php">Query 1</a>
+            <a href="query2.php">Query 2</a>
+            <a href="query3.php">Query 3</a>
+          </div>
+          <div class="sidenavlinks">
+            <a href="https://github.com/mm1089/advanced-databases-group/">GitHub</a>
+            <a href="https://github.com/mm1089/advanced-databases-group/blob/main/README.md">Read the Docs</a>
+          </div>
+        <body>
+            <div class="content">
+                <h1></h1>
+            
+            </div>
+        </body>
+    </main>
+</html>
+
+<!-- Resources Used -->
+<!-- http://php.net/manual/en/faq.installation.php#faq.installation.addtopath -->
+<!-- For Issue #1 'bash: php: command not found' -->
