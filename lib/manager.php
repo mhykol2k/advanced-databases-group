6135185029f3fd7330c8cea0915ff97d6ef9379a
mm1089 <michael@mclain.co.uk>
2022-05-11 14:03:25 +0100

    <edit> stying

--- lib/manager.php
+++ lib/manager.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Table Manager</title>
+        <title>Manager</title>
         <link rel="stylesheet" href="../assets/main.css">
         <link rel="icon" href="../media/globe-icon.png">
     </head>
@@ -12,14 +12,16 @@
         <body>
 <!-- Navigation bar -->
         <div class="sidenav">
-            <h2 class="title">🧑‍💼Manager Page</h2>
+            <h2 class="title">Manager Page</h2>
             <a href="index.php">Return to Home</a>
-            <a href="https://github.com/mm1089/advanced-databases-group">GitHub</a>
-            <a href="https://github.com/mm1089/advanced-databases-group/blob/main/README.md">Read the Docs</a>
+        <div class="sidenavlinks">
+            <a href="https://github.com/mm1089/advanced-databases-group/">GitHub</a>
+            <a href="https://github.com/mm1089/advanced-databases-group/blob/main/README.md#prerequisites">Read the Docs</a>
+        </div>
+
         </div>
         <div class="content">
-            <h1>Create new Record</h1>
-            <h5>Users will manage records here</h5>
+            <h1>Un-used</h1>
         </div>
 
 <!-- End of Body -->
