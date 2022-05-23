314603694a2057bd298478ff3782aab3f31c17fe
Will <wc104@canterbury.ac.uk>
2022-05-23 19:13:08 +0100

    <mod>updated script

--- scripts/SQL_Query_1.sql
+++ scripts/SQL_Query_1.sql
@@ -1,4 +1,4 @@
 /* Query 1 - All employees under certain manager EMPNO */
 SELECT EMP.ENAME
 FROM assignment.EMP
-WHERE EMP.NAME NOT IN (SELECT MGR = '7839' FROM assignment.EMP)
+WHERE EMP.NAME IN (SELECT MGR = '7839' FROM assignment.EMP)
